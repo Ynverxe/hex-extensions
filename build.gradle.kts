1df2b50a47226585e4a46b54f8366fa227f05b1f
Ynverxe <nahubar65@gmail.com>
2025-07-06 20:12:01 -0300

    build: replace variable 'version' in config files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,15 @@
-subprojects {
-    group = "io.github.ynverxe.hex-extensions"
+subprojects.forEach { subproject ->
+    subproject.group = "io.github.ynverxe.hex-extensions"
+
+    subproject.plugins.withId("java") {
+        subproject.tasks.named<ProcessResources>("processResources") {
+            filteringCharset = "UTF-8"
+
+            filesMatching("extension.conf") {
+                expand(
+                    "version" to subproject.version
+                )
+            }
+        }
+    }
 }
