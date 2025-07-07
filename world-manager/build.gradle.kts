15cdf40eb795a17e7e7428e475a6431612c9fdb7
Ynverxe <nahubar65@gmail.com>
2025-07-06 21:15:32 -0300

    build: add luck perms to world-manager execution

--- world-manager/build.gradle.kts
+++ world-manager/build.gradle.kts
@@ -9,3 +9,7 @@ dependencies {
     implementation("dev.hollowcube:polar:1.14.2")
     implementation("org.spongepowered:configurate-yaml:4.0.0")
 }
+
+tasks.runHexServer {
+    addExtensionFromTask(":luckperms-as-extension:shadowJar")
+}
